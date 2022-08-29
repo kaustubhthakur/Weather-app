c15431cda72d8825ad4b6294055c7d757419caa1
kaustubh thakur <93928807+kaustubhthakur@users.noreply.github.com>
2022-08-29 13:32:03 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c0912a15d9msh22cda8aac17bffap17ca2fjsnf71c9aa96887',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "fae34f1570da5066c67feffe792fb146"
